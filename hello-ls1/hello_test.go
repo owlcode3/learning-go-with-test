1aa26e9eefdf130e18567d3ca99437043866d687
owlCode <akoladeo890@yahoo.com>
2024-06-18 21:01:16 +0100

    Modify hello_test.go file to include test for when empty string is supplied

--- hello-ls1/hello_test.go
+++ hello-ls1/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("Saying hello to people", func(t *testing.T) {
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+	})
+
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
