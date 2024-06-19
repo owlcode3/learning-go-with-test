97e2022efd0321325e8011dff8341e5efa6e9deb
owlCode <akoladeo890@yahoo.com>
2024-06-19 12:03:05 +0100

    Add option to pick what language to be greeted in

--- hello-ls1/hello_test.go
+++ hello-ls1/hello_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestHello(t *testing.T) {
 	t.Run("Saying hello to people", func(t *testing.T) {
 
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
@@ -14,10 +14,24 @@ func TestHello(t *testing.T) {
 
 	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
 		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Antoine", "French")
+		want := "Bonjour, Antoine"
+		assertCorrectMessage(t, got, want)
+
 	})
 }
 func assertCorrectMessage(t testing.TB, got, want string) {
