a9ed979bcd858ac25bf62b1117f326e881a61157
owlCode <akoladeo890@yahoo.com>
2024-06-19 01:05:39 +0100

    Refactor the code in hello_test.go

--- hello-ls1/hello_test.go
+++ hello-ls1/hello_test.go
@@ -8,9 +8,7 @@ func TestHello(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 
 	})
 
@@ -19,8 +17,12 @@ func TestHello(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
