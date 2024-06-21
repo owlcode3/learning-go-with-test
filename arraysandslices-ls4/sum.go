e0ba51f1b6a49a5b9741330383653980107c432c
owlCode <akoladeo890@yahoo.com>
2024-06-21 14:03:46 +0100

    Implement functionality to add the sum of each slice passed into the function and return all the result in a new slice

--- arraysandslices-ls4/sum.go
+++ arraysandslices-ls4/sum.go
@@ -1,7 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(SumAll([]int{1, 2, 3, 4}, []int{5, 6, 7, 8, 9}))
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+
+	sums := make([]int, len(numbersToSum))
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
 }
 
 func Sum(numbers []int) (sum int) {
--- arraysandslices-ls4/sum_test.go
+++ arraysandslices-ls4/sum_test.go
@@ -1,9 +1,22 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
 
 func TestSum(t *testing.T) {
-	t.Run("collections of 5 numbers: Array", func(t *testing.T) {
+	t.Run("collection of 5 numbers", func(t *testing.T) {
 
 		numbers := []int{1, 2, 3, 4, 5}
 		got := Sum(numbers)
