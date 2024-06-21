518c584943faec85e9f8190e8aff86ee01f3c565
owlCode <akoladeo890@yahoo.com>
2024-06-21 15:00:00 +0100

    Implement functionality to add the sum of each slice minus the first number (slice[1:]) passed into the function and return all the result in a new slice. Zero will be returned if slice is empty

--- arraysandslices-ls4/sum.go
+++ arraysandslices-ls4/sum.go
@@ -3,16 +3,35 @@ package main
 import "fmt"
 
 func main() {
+
 	fmt.Println(SumAll([]int{1, 2, 3, 4}, []int{5, 6, 7, 8, 9}))
+	fmt.Println(SumAllTails([]int{}, []int{0, 9}))
+
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tails := numbers[1:]
+			sums = append(sums, Sum(tails))
+		}
+
+	}
+
+	return sums
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-
 	sums := make([]int, len(numbersToSum))
 
 	for i, numbers := range numbersToSum {
 		sums[i] = Sum(numbers)
 	}
+
 	return sums
 }
 
@@ -20,5 +39,6 @@ func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
 	}
+
 	return
 }
