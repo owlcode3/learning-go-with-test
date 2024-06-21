b8e21474be3182a7cc935d879b65f2268b7fecdf
owlCode <akoladeo890@yahoo.com>
2024-06-21 10:49:52 +0100

    Add the functionality to repeat a character in a specified no of times

--- iteration-ls3/repeat_test.go
+++ iteration-ls3/repeat_test.go
@@ -1,12 +1,47 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("expected result", func(t *testing.T) {
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("expected no of time character was repeated", func(t *testing.T) {
+		noOfTime := 5
+		repeated := Repeat("a", noOfTime)
+
+		if noOfTime != len(repeated) {
+			t.Errorf("expected %d but got %d", noOfTime, len(repeated))
+		}
+	})
+}
+
+func Repeat(character string, noOfTime int) (repeated string) {
+	for i := 0; i < noOfTime; i++ {
+		repeated += character
 	}
+	return
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	result := Repeat("b", 5)
+	fmt.Println(result)
+	// Output: bbbbb
+
 }
